f18da23ce8f46bb7b2273d872d4809cf1b9fbbcc
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-05-27 19:18:06 +0530

    file added

--- src/main/java/org/Myapp/ConstructorInjection.java
+++ src/main/java/org/Myapp/ConstructorInjection.java
@@ -1,4 +1,13 @@
 package org.Myapp;
 
 public class ConstructorInjection {
+    private Service service;
+    public Controller(Service service){
+        this.service=service;
+    }
+    public void handleRequest(){
+        service.doSomething();
+    }
 }
+
+
